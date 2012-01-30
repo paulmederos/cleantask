0bf1d0eb119a6defa6ea779099b18fc27c85542c
Paul Mederos <paul@primeenterprise.net>
2012-01-30 14:22:21 -0500

    Trying to figure out the fields_for builder for Inspection and Questions. Until then, I'll do a hard-coded inspection.

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,6 +1,5 @@
 class Inspection < ActiveRecord::Base
   has_many :questions, :dependent => :destroy
-  accepts_nested_attributes_for :questions
   
-  attr_accessible :name
+  accepts_nested_attributes_for :questions #, :reject_if => proc { |attributes| attributes['content'].blank? }
 end
