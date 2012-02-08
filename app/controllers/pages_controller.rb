3aba1d46d709a0eee5e300bb28ada44ebce526fe
Paul Mederos <paul@primeenterprise.net>
2012-02-07 21:39:19 -0500

    Minor login view changes.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,23 @@
 class PagesController < ApplicationController
   
   def home
-    @clients = current_user.clients.find(:all, :order => "created_at DESC")
-    @facilities = []
-    @clients.each do |client|
-      @facilities += client.facilities.find(:all, :order => "name ASC")
+    if current_user
+      @clients = current_user.clients.find(:all, :order => "created_at DESC")
+      @facilities = []
+      @clients.each do |client|
+        @facilities += client.facilities.find(:all, :order => "name ASC")
+      end
+    
+      @open_notices =[]
+      @closed_notices = []
+    
+      @facilities.each do |facility|
+        @open_notices += facility.notices.find_all_by_status("Open", :order => "priority ASC, created_at DESC")
+        @closed_notices += facility.notices.find_all_by_status("Closed", :order => "created_at DESC")
+      end
+    
+      @open_notices.sort! {|a,b| b.created_at <=> a.created_at }
+      @closed_notices.sort! {|a,b| b.created_at <=> a.created_at }
     end
-    
-    @open_notices =[]
-    @closed_notices = []
-    
-    @facilities.each do |facility|
-      @open_notices += facility.notices.find_all_by_status("Open", :order => "priority ASC, created_at DESC")
-      @closed_notices += facility.notices.find_all_by_status("Closed", :order => "created_at DESC")
-    end
-    
-    @open_notices.sort! {|a,b| b.created_at <=> a.created_at }
-    @closed_notices.sort! {|a,b| b.created_at <=> a.created_at }
   end
 end
